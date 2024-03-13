b61beca31ca29dca3e191479a762cf6543b62c31
dev-chpark <xpmxf5@gmail.com>
2024-03-13 12:02:46 +0900

    🎉 init: Initial Commit

---
+++ src/main/java/com/example/springedu/SpringEduApplication.java
@@ -0,0 +1,13 @@
+package com.example.springedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEduApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springedu/SpringEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springedu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEduApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
